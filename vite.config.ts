2bfc3176230954ad443ce0f026c01781d6432689
Dan Knob <dknob@lus-hmhvh7fp2r.tailf4adb.ts.net>
2026-05-28 00:12:00 -0700

    vite thing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  base: '/',                    // ← changed this
+  base: '/',                    // ← correct for custom domain
   plugins: [react(), tailwindcss()],
 })
